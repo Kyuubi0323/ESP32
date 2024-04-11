92605510829ff438b51a91aba778167ff3dbef13
Kyuubi <khoi.nv202647@sis.hust.edu.vn>
2024-04-11 14:27:29 +0700

    add sta_wifi

--- ESP_Library_Common/common/wifi/wifi_sta_hadilao.h
+++ ESP_Library_Common/common/wifi/wifi_sta_hadilao.h
@@ -10,8 +10,30 @@
  * @copyright Copyright (c) 2024
  * 
  */
-#include "esp_err.h"
-#include "hal/gpio_types.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "freertos/queue.h"
+#include "freertos/ringbuf.h"
+#include "freertos/semphr.h"
+
+#include "nvs_flash.h"
+
+#include "esp_log.h"
+#include "esp_wifi.h"
+#include "esp_smartconfig.h"
+#include "esp_spiffs.h"
+#include "esp_http_client.h"
+#include "esp_attr.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "esp_http_server.h"
+#include "esp_mac.h"
+#include "esp_chip_info.h"
+
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "driver/spi_master.h"
 
 #include <math.h>
 #include <stdint.h>
@@ -21,7 +43,9 @@
 #include <stdlib.h>
 
 void wifi_init(void);
-void wifi_sta(wifi_config_t wifi_cfg, wifi_mode_t wifi_mode);
+void wifi_start(wifi_config_t wifi_cfg, wifi_mode_t wifi_mode);
+void chip_stats(void);
+
 
 #endif
  
