a46e4fe9ac0eab6cfdb80ca057b340397f4aa5c2
Kyuubi <khoi.nv202647@sis.hust.edu.vn>
2024-04-11 10:33:07 +0700

    Doxygen Template

--- ESP_Library_Common/common/output/output_hadilao.h
+++ ESP_Library_Common/common/output/output_hadilao.h
@@ -1,6 +1,15 @@
 #ifndef _OUTPUT_HADILAO_H_
 #define _OUTPUT_HADILAO_H_
-
+/**
+ * @file output_hadilao.h
+ * @Kyuubi {Kyuubi} ({khoi.nv0323.work@gmail.com})
+ * @brief 
+ * @version 0.1
+ * @date 2024-04-11
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #include "esp_err.h"
 #include "hal/gpio_types.h"
 
---
+++ ESP_Library_Common/common/wifi/wifi_hadilao.h
@@ -0,0 +1,18 @@
+#ifndef _WIFI_HADILAO_H_
+#define _WIFI_HADILAO_H_
+/**
+ * @file wifi_hadilao.h
+ * @Kyuubi {Kyuubi} ({khoi.nv0323.work@gmail.com})
+ * @brief 
+ * @version 0.1
+ * @date 2024-04-11
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include "esp_err.h"
+#include "hal/gpio_types.h"
+
+
+
+#endif 
