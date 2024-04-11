375a70a6a9cf52d6ea7cd9dbd3088ca847cfeff3
Kyuubi <khoi.nv202647@sis.hust.edu.vn>
2024-04-11 14:27:46 +0700

    update app_main

--- ESP_Library_Common/main/app_main.c
+++ ESP_Library_Common/main/app_main.c
@@ -1,19 +1,63 @@
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
-#include "sdkconfig.h"
+#include "freertos/event_groups.h"
+#include "freertos/queue.h"
+#include "freertos/ringbuf.h"
+#include "freertos/semphr.h"
+
+#include "nvs_flash.h"
 
 #include "esp_log.h"
+#include "esp_wifi.h"
+#include "esp_smartconfig.h"
+#include "esp_spiffs.h"
+#include "esp_http_client.h"
+#include "esp_attr.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "esp_http_server.h"
+#include "esp_mac.h"
+#include "esp_chip_info.h"
 
-#include "sdkconfig.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "driver/spi_master.h"
+
 #include "output_hadilao.h"
-//#include "input_hadilao.h"
+#include "input_hadilao.h"
+#include "wifi_sta_hadilao.h"
+
+
 #define LED 2
 static const char *TAG = "MAIN";
+char ssid[50] = {0};
+char pwd[50] = {0};
+
 void app_main()
-{
+{   
+    esp_err_t err;
+    ESP_LOGI(TAG, "Initializing...");
+    err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+    /* chip's information*/
+    //chip_stats();
+    /* init GPIO */
     output_io_create(LED);
+    /* init wifi configuration*/
+    wifi_init();
+    sprintf(ssid, "Kyuubi");
+    sprintf(pwd, "laclac123");
+    wifi_config_t wifi_config;
+    bzero(&wifi_config, sizeof(wifi_config_t));
+    memcpy(wifi_config.sta.ssid, ssid, strlen(ssid));
+    memcpy(wifi_config.sta.password, pwd, strlen(pwd));
+    
+    wifi_start(wifi_config, WIFI_MODE_STA);
 
     while(1)
     {
