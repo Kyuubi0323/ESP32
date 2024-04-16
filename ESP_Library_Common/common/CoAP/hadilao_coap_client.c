aa7cba3d3396cacb4b3e52fb268321bffc7eb178
Kyuubi <khoi.nv202647@sis.hust.edu.vn>
2024-04-16 08:44:29 +0700

    add_coAP

---
+++ ESP_Library_Common/common/CoAP/hadilao_coap_client.c
@@ -0,0 +1,79 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "freertos/queue.h"
+#include "freertos/ringbuf.h"
+#include "freertos/semphr.h"
+
+#include "nvs_flash.h"
+
+#include "esp_log.h"
+#include "esp_wifi.h"
+#include "esp_smartconfig.h"
+#include "esp_spiffs.h"
+#include "esp_http_client.h"
+#include "esp_attr.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "esp_http_server.h"
+#include "esp_mac.h"
+#include "esp_chip_info.h"
+
+//#include "coap3/coap.h"
+
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "driver/spi_master.h"
+
+#include <math.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <sys/param.h>
+
+#include "hadilao_coap_client.h"
+
+const static char *TAG = "CoAP_client";
+
+// static int resp_wait = 1;
+// static coap_optlist_t *optlist = NULL;
+// static int wait_ms;
+
+// coap_response_t
+// message_handler(coap_session_t *session,
+//                 const coap_pdu_t *sent,
+//                 const coap_pdu_t *received,
+//                 const coap_mid_t mid)
+// {
+//     const unsigned char *data = NULL;
+//     size_t data_len;
+//     size_t offset;
+//     size_t total;
+//     coap_pdu_code_t rcvd_code = coap_pdu_get_code(received);
+
+//     if (COAP_RESPONSE_CLASS(rcvd_code) == 2) {
+//         if (coap_get_data_large(received, &data_len, &data, &offset, &total)) {
+//             if (data_len != total) {
+//                 printf("Unexpected partial data received offset %u, length %u\n", offset, data_len);
+//             }
+//             printf("Received:\n%.*s\n", (int)data_len, data);
+//             resp_wait = 0;
+//         }
+//         return COAP_RESPONSE_OK;
+//     }
+//     printf("%d.%02d", (rcvd_code >> 5), rcvd_code & 0x1F);
+//     if (coap_get_data_large(received, &data_len, &data, &offset, &total)) {
+//         printf(": ");
+//         while(data_len--) {
+//             printf("%c", isprint(*data) ? *data : '.');
+//             data++;
+//         }
+//     }
+//     printf("\n");
+//     resp_wait = 0;
+//     return COAP_RESPONSE_OK;
+// }
---
+++ ESP_Library_Common/common/CoAP/hadilao_coap_server.h
@@ -0,0 +1,44 @@
+#ifndef _HADILAO_COAP_SERVER_H_
+#define _HADILAO_COAP_SERVER_H_
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "freertos/queue.h"
+#include "freertos/ringbuf.h"
+#include "freertos/semphr.h"
+
+#include "nvs_flash.h"
+
+#include "esp_log.h"
+#include "esp_wifi.h"
+#include "esp_smartconfig.h"
+#include "esp_spiffs.h"
+#include "esp_http_client.h"
+#include "esp_attr.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "esp_http_server.h"
+#include "esp_mac.h"
+#include "esp_chip_info.h"
+
+//#include "coap3/coap.h"
+
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "driver/spi_master.h"
+
+#include <math.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <sys/param.h>
+
+
+
+
+#endif
